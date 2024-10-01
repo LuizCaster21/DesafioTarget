d08d0a0332ae4816bf0933348c3b6f26913c3712
LuizCaster <luizfernandovc21@gmail.com>
2024-10-01 00:46:33 -0300

    Desafios concluidos

---
+++ Questao_1/Questao_1/Questao_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Questao_1
+{
+    class Program
+    {
+
+
+        public static void Main(string[] args)
+
+        {
+            int a = 0, b = 1;
+            int fibonacci = 0;
+            int x, cont = 0;
+
+            Console.WriteLine("Informe o número desejado para verificação.");
+            x = int.Parse(Console.ReadLine());
+
+            while (fibonacci <= x )
+            {
+                if(x == a)
+                {
+                    Console.WriteLine($"O número {x} faz parte da sequência de fibonacci.");
+                }  else if (cont >= x && x != a && x != b && x != fibonacci)
+                   {
+                    Console.WriteLine($"O número {x} não faz parte da sequência de fibonacci.");
+                    break;
+                   }
+
+                fibonacci = a + b;
+                a = b;
+                b = a;
+                cont++;
+            }
+
+            
+        }
+    }
+}
---
+++ Questao_2/Questao_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Questao_2
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            string input;
+            int cont = 0;
+
+            Console.WriteLine("realizar entrada de dados.");
+            input = Console.ReadLine();
+
+            foreach (char c in input)
+            {
+                if (c == 'a' || c == 'A')
+                {
+                    cont++;
+                }
+            }
+            
+            if (cont > 0)
+            {
+                Console.WriteLine($"A letra 'a' aparece {cont} vezes.");
+            } else
+            {
+                Console.WriteLine("A letra 'a' não aparece na entrada de dados.");
+            }
+
+        }
+    }
+}
